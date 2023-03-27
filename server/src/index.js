b5bc9275c4cdd4e639968a347077a20887f182c7
Sushant Sharma <ssushantlb08@gmail.com>
2023-03-27 23:01:01 +0530

    added db connection

--- server/src/index.js
+++ server/src/index.js
@@ -1,13 +1,16 @@
 import express from "express";
-import mongoose from 'mongoose';
 import cors from 'cors';
+import mongoose from 'mongoose';
 // import { UserModel } from "./models/Users";
+import {UserRouter} from './routes/users.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-mongoose.connect("mongodb+srv://sushantsharmadev:MERNpassword@recipes.nkwdj8p.mongodb.net/recipes?retryWrites=true&w=majority")
+app.use("/auth",UserRouter)
+
+mongoose.connect("mongodb+srv://sushantsharmadev:Qwerty123@recipe.6i7hjnb.mongodb.net/recipe?retryWrites=true&w=majority")
 
 app.listen(3001,()=>{console.log('server is up');})
