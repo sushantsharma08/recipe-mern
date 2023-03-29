ba2c564366c0e8f8ed4b6afd22253536a49faa99
Sushant Sharma <ssushantlb08@gmail.com>
2023-03-30 01:07:25 +0530

    creating ui

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,11 +1,26 @@
 import { useState } from 'react'
 import './App.css'
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom"
+import Home from './pages/Home'
+import SavedRecipe from './pages/SavedRecipe'
+import CreateRecipe from './pages/CreateRecipe'
+import Authentication from './pages/Authentication'
+import Navbar from './components/Navbar'
 
 function App() {
   const [count, setCount] = useState(0)
 
   return (
     <div className="App">
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route path='/' element={<Home />} />
+          <Route path='/auth' element={<Authentication />} />
+          <Route path='/saved-recipes' element={<SavedRecipe />} />
+          <Route path='/create-recipe' element={<CreateRecipe />} />
+        </Routes>
+      </Router>
     </div>
   )
 }
