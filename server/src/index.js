ebc6b22621b9804303f05a462e34e98805e63c03
Sushant Sharma <ssushantlb08@gmail.com>
2023-03-31 02:30:37 +0530

    recipes post and get apis

--- server/src/index.js
+++ server/src/index.js
@@ -3,6 +3,7 @@ import cors from 'cors';
 import mongoose from 'mongoose';
 // import { UserModel } from "./models/Users";
 import {UserRouter} from './routes/users.js'
+import {recipesRouter} from './routes/recipes.js'
 
 const app = express();
 
@@ -10,6 +11,7 @@ app.use(express.json());
 app.use(cors());
 
 app.use("/auth",UserRouter)
+app.use("/recipes",recipesRouter)
 
 mongoose.connect("mongodb+srv://sushantsharmadev:Qwerty123@recipe.6i7hjnb.mongodb.net/recipe?retryWrites=true&w=majority")
 
