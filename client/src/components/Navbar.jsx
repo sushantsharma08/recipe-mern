69e1e16ab3fa5dde00ce0b65f7fc330d2a154da1
Sushant Sharma <ssushantlb08@gmail.com>
2023-03-31 01:07:03 +0530

    login/logout btn functionality

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
+import { useCookies } from "react-cookie"
+import {useNavigate} from "react-router-dom"
+
 
 const Navbar = () => {
+  const [cookies,setCookies]=useCookies(["access_token"]);
+  const navigate =  useNavigate();
+  const logout=()=>{
+    setCookies("access_token","");
+    window.localStorage.removeItem("userId");
+    navigate("/auth")
+  }
   return (
     <div className="navbar">
     <Link to="/">Home</Link>
     <Link to="/create-recipe">Create Recipe</Link>
     <Link to="/saved-recipes">Saved Recipes</Link>
-      <Link to="/auth">Login/Register</Link> 
+       
+      {!cookies.access_token?<Link to="/auth">Login/Register</Link>:(<button className='logout' onClick={logout}>Logout</button>)}
   </div>
   )
 }
