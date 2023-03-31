aff77442aa1ec602bc7bc43096d13cdd9c035b68
Sushant Sharma <ssushantlb08@gmail.com>
2023-04-01 01:38:27 +0530

    recipe apis done

--- server/src/routes/recipes.js
+++ server/src/routes/recipes.js
@@ -1,8 +1,8 @@
 
 import express from "express";
 import mongoose from "mongoose";
-
 import { RecipeModel } from "../models/Recipes.js";
+import { UserModel } from "../models/Users.js";
 
 const router = express.Router();
 
@@ -25,4 +25,36 @@ router.post("/",async (req,res)=>{
     }
 })
 
+router.put("/",async (req,res)=>{
+    try {
+        const recipe = await RecipeModel.findById(req.body.recipeId)
+        const user = await UserModel.findById(req.body.userId)
+        user.savedRecipes.push(recipe);
+        await user.save();
+        res.json({savedRecipes:user.savedRecipes});
+    } catch (err) {
+        res.json(err)
+    }
+});
+
+router.get("/savedRecipes/ids",async(req,res)=>{
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        res.json({savedRecipes:user.savedRecipes})
+    } catch (error) {
+        console.error(error);
+    }
+})
+
+router.get("/savedRecipes", async (req,res)=>{
+    try {
+        const user = await UserModel.findById(req.body.userId);
+        const savedRecipes= await RecipeModel.find({
+            _id:{$in:user.savedRecipes}
+        })
+        res.json({savedRecipes})
+    } catch (error) {
+        console.error(error);
+    }
+})
 export {router as recipesRouter}
