67f2d3a65050312af6006aafa8798ed75e30da04
Sushant Sharma <ssushantlb08@gmail.com>
2023-04-02 21:03:25 +0530

    update backend

--- server/src/index.js
+++ server/src/index.js
@@ -2,17 +2,13 @@ import express from "express";
 import cors from 'cors';
 import mongoose from 'mongoose';
 // import { UserModel } from "./models/Users";
-import {UserRouter} from './src/routes/users.js'
-import {recipesRouter} from './src/routes/recipes.js'
+import {UserRouter} from './routes/users.js'
+import {recipesRouter} from './routes/recipes.js'
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
-
-app.use("/",(req,res)=>{
-    res.json({message:"hello server"})
-})
 app.use("/auth",UserRouter)
 app.use("/recipes",recipesRouter)
 
