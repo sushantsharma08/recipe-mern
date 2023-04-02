6f83b0d67bc6728bbd7d5c1902e6242586b22dba
Sushant Sharma <ssushantlb08@gmail.com>
2023-04-02 18:18:25 +0530

    Update index.js

--- server/src/index.js
+++ server/src/index.js
@@ -10,7 +10,9 @@ const app = express();
 app.use(express.json());
 app.use(cors());
 
-
+app.use("/",(req,res)=>{
+    res.json({message:"hello server"})
+})
 app.use("/auth",UserRouter)
 app.use("/recipes",recipesRouter)
 
